a15832ea1e9173df5a18bc8bf4a24a4964c14040
Filip Lipiński <filip.lipinski@outlook.com>
2019-09-29 17:47:54 +0200

    Add db to config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db: '',
+    db: 'mongodb+srv://admin:admin123@cluster0-bo4cy.mongodb.net/cms-express-mongodb?retryWrites=true&w=majority',
     keySession: ['CMS-EXPRESS-MONGODB-KEY'],
     maxAgeSesion: 24 * 60 * 60 * 1000 //24hours
 }
